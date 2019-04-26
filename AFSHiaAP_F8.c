15ee875a8677df579a2905cfdb33137097de2f77
zakythr <37214711+zakythr@users.noreply.github.com>
2019-04-26 23:27:48 +0700

    Update AFSHiaAP_F8.c

--- AFSHiaAP_F8.c
+++ AFSHiaAP_F8.c
@@ -10,8 +10,16 @@
 
 char password[100]={"qE1~ YMUR2\"`hNIdPzi\%^t@(Ao:=CQ,nx4S[7mHFye#aT6+v)DfKL$r?bkOGB>}!9_wV']jcp5JZ&Xl|\\8s;g<{3.u*W-0"};
 
-static const char *dirpath = "/home/naim/shift4";
+static const char *dirpath = "/home/zaky/shift4";
 int key=17;
+
+FILE *f2;
+FILE *cp2;
+char cek1[500];
+char cek2[500];
+char namalama[100];
+char namabaru[100];
+char copy[]=".copy";
 
 void dekript(char *simpanpath)
 {
@@ -35,7 +43,7 @@ void dekript(char *simpanpath)
 
 void enkript(char *simpanpath)
 {
-  int panjangpath, i, j;
+  int panjangpath;
   panjangpath = strlen(simpanpath);
 
   for(int i=0; i < panjangpath; i++)
@@ -134,10 +142,96 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 	return res;
 }
 
+static int xmp_write(const char *path, const char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi)
+{
+	char *ekstensi;
+	time_t formatime;
+	struct tm *info;
+	char path2[500];
+	char waktu[500];
+
+	char fpath[1000];
+
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+
+	int fd;
+	int res;
+
+	(void) fi;
+	
+	
+	fd = open(fpath, O_WRONLY);
+	
+
+	if (fd == -1)
+		return -errno;
+
+	if(strstr(path,copy) != NULL){
+		return -errno;
+	}
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+
+	f2 = fopen (fpath, "r");
+
+	fgets(cek2, 500, f2);
+
+	if(strcmp(cek1,cek2)!=0){
+		char* argv[3] = {"mkdir", "/home/zaky/Downloads/BackUp", NULL};
+		execv("/bin/mkdir", argv);
+		char format[100]="/home/zaky/Downloads/BackUp";
+		opendir(format);
+		time(&formatime);
+		info = localtime(&formatime);
+		sprintf(waktu, "--%04d--%02d--%02d__%02d:%02d:%02d", info->tm_year, info->tm_mon, info->tm_mday, info->tm_hour, info->tm_min, info->tm_sec);
+		ekstensi=strchr(path, '.');
+		sprintf(path2, "%s", path);
+		int i=strlen(path2)-4;
+		if(path2[i]=='.')path2[i]='\0';
+		sprintf(namalama,"/home/zaky/Downloads%s",path);
+		sprintf(namabaru,"/home/zaky/Downloads/BackUp%s__%s%s", path2, waktu, ekstensi);
+		cp2 = fopen(namabaru,"w+");
+		fprintf(cp2, "%s", cek2);
+		fclose(cp2);
+	}
+
+
+	fclose(f2);
+	close(fd);
+
+	return res;
+}
+
+static int xmp_truncate(const char *path, off_t size)
+{
+
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	
+	int res;
+
+	res = truncate(fpath, size);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
 static struct fuse_operations xmp_oper = {
 	.getattr	= xmp_getattr,
 	.readdir	= xmp_readdir,
 	.read		= xmp_read,
+	.write		= xmp_write,
+	.truncate	= xmp_truncate,
 };
 
 int main(int argc, char *argv[])
